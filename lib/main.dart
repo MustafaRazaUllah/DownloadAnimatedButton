f5502eb82267df3068ef012990df4ead634733ec
Mustafa Raza <mustafaraza@Mustafas-MacBook-Pro.local>
2023-09-07 15:11:49 +0500

    Bug fixes and update code on repo

--- lib/main.dart
+++ lib/main.dart
@@ -3,51 +3,48 @@ import 'package:flutter/material.dart';
 import 'ButtonAnimation.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const HomePage());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ButtonAnimation(
-              Color.fromRGBO(57, 92, 249, 1),
-              Color.fromRGBO(44, 78, 233, 1),
+              Colors.purple[700]!,
+              Colors.purple[800]!,
             ),
-            // const SizedBox(
-            //   height: 20,
-            // ),
-            // ButtonAnimation(Colors.yellow[700]!, Colors.yellow[800]!),
-            // const SizedBox(
-            //   height: 20,
-            // ),
-            // ButtonAnimation(Colors.green[400]!, Colors.green[600]!),
-            // const SizedBox(
-            //   height: 20,
-            // ),
-            // ButtonAnimation(Colors.red[700]!, Colors.red[800]!),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonAnimation(
+              Colors.yellow[700]!,
+              Colors.yellow[800]!,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonAnimation(
+              Colors.green[400]!,
+              Colors.green[600]!,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonAnimation(
+              Colors.red[700]!,
+              Colors.red[800]!,
+            ),
           ],
         ),
       ),
